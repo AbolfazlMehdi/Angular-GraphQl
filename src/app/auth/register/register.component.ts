f733c6ba759c38be1aa91bf832bb62fc847043bb
Mehdabol <Mehdiabolfazl73@gmail.com>
2024-02-29 21:47:20 +0330

    add registry form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent  {
+  registerForm = new FormGroup({
+    userName: new FormControl('' , Validators.required),
+    firstName: new FormControl('' , Validators.required),
+    lastName: new FormControl('' , Validators.required),
+    mobileNumber: new FormControl(''),
+    password: new FormControl('' , Validators.required),
+    confirmPassword: new FormControl('' , Validators.required),
+  });
+  constructor() {
+  }
+
+  onSubmitFor(): void{
+    if (this.registerForm.invalid){
+      this.registerForm.markAllAsTouched();
+    }
+   const form =  this.registerForm.getRawValue();
+  }
 
 }
